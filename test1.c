ace80933cd59d44b9654f441367f1dd46b6b27c6
zccxl <93762209+zccxl@users.noreply.github.com>
2021-11-08 19:18:52 +0800

    第一次修改test1.c文件
    第一次用githup修改test1.c文件

--- test1.c
+++ test1.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main(void)
 {
+  char name[20];
   printf("Hello,world!");
+  printf("input yor name:");
+  scanf("%s",&name);
+  printf("your name is %s",name);
   return 0;
 }
